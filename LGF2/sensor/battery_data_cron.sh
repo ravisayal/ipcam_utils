7a7237d5a65dcccf1bad9dc856c7342ea439f284
Ravinder <ravisayal@yahoo.com>
2021-04-04 12:14:23 -0400

    updated cron script for output file location

--- LGF2/sensor/battery_data_cron.sh
+++ LGF2/sensor/battery_data_cron.sh
@@ -1,3 +1,15 @@
+#!/bin/bash 
+progname=$(basename $0)
+script_call_dir=${0%$progname}
+script_curr_dir=${script_call_dir:-$pwd/}
+script_call_string="$progname $@"
+
+echo "progname:"$progname
+echo "script_call_dir:"$script_call_dir
+echo "script_curr_dir:"$script_curr_dir
+echo "script_call_string:"$script_call_string
+
+
 now=$(date +"%m/%d/%Y %H:%M:%S");  \
 # fetch the battery level and save in bl variable \
 bl=`curl 'http://192.168.1.210:8080/sensors.json?from=1610841254704&sense=battery_level' \
@@ -23,4 +35,4 @@ bt=`curl 'http://192.168.1.210:8080/sensors.json?from=1612142935259&sense=batter
   --insecure` ; \
  bt="${bt:52:8}" ; \
  bt=`echo $bt | tr -dc '[:alnum:].'` ;\
- echo "$now"",""$bl"",""$bt" >> battery_data.csv ; 
+ echo "$now"",""$bl"",""$bt" >> $script_curr_dir/battery_data.csv ; 
