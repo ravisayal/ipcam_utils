46b62e720c257f4608fd6226814b3faf1bba7b7b
Ravinder <ravisayal@yahoo.com>
2021-04-04 18:24:09 -0400

    updated Ip address for z958

--- z958/sensor/battery_data_cron.sh
+++ z958/sensor/battery_data_cron.sh
@@ -13,12 +13,12 @@ echo "script_call_string:"$script_call_string
 now=$(date +"%m/%d/%Y %H:%M:%S");  
 echo "Current Time: "$now
 # fetch the battery level and save in bl variable \
-bl=`curl 'http://192.168.1.210:8080/sensors.json?from=1610841254704&sense=battery_level' \
+bl=`curl 'http://192.168.1.104:8080/sensors.json?from=1610841254704&sense=battery_level' \
   -H 'Connection: keep-alive' \
   -H 'Accept: */*' \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36' \
   -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'Referer: http://192.168.1.210:8080/sensors.html' \
+  -H 'Referer: http://192.168.1.104:8080/sensors.html' \
   -H 'Accept-Language: en-US,en;q=0.9' \
   --compressed \
   --insecure` ; 
@@ -28,7 +28,7 @@ bl=`curl 'http://192.168.1.210:8080/sensors.json?from=1610841254704&sense=batter
 
 
 # fetch the battery temp and save in bt variable \
-bt=`curl 'http://192.168.1.210:8080/sensors.json?from=1612142935259&sense=battery_temp' \
+bt=`curl 'http://192.168.1.104:8080/sensors.json?from=1612142935259&sense=battery_temp' \
   -H 'Connection: keep-alive' \
   -H 'Accept: */*' \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36' \
